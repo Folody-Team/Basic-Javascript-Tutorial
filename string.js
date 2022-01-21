cc7ee4d3d39fb5f3c3aa617d7ae9d94e8d178ebc
MorgTheC <91877913+MorgTheC@users.noreply.github.com>
2022-01-21 21:10:08 +0700

    Add some string

---
+++ string.js
@@ -0,0 +1,12 @@
+/*In basic.js, we have learn about how to declare variable 
+*In this file we will learn about string in javascript
+*javascript strings are for storing and manipulating text
+*A javascripst string is a text inside a quote , yes it can be 0 character ( blank)
+*/
+
+//String example
+
+const name = "Bob";
+
+//you can also use single quote as well:
+const name = 'bob';
