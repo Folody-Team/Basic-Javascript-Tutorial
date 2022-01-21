58d462bbc43ed4416b6c4202cdf3ae569c69015c
Từ Thắng Phát <81029660+phatdev12@users.noreply.github.com>
2022-01-21 21:48:03 +0700

    Create Class.js

---
+++ Class.js
@@ -0,0 +1,16 @@
+// for example in class
+class LogName {
+
+  constructor(name) {
+    this.name = name;
+  }
+
+  log() {
+    console.log(this.name);
+  }
+
+}
+
+
+let user = new LogName("Folody");
+user.log();
