b028563c4cd2c7ae271b18630a358a29619fa399
MorgTheC <91877913+MorgTheC@users.noreply.github.com>
2022-01-21 21:33:34 +0700

    Create function.js

---
+++ function.js
@@ -0,0 +1,21 @@
+//In this file we learn how to create javascript function:
+
+//JavaScript functions are defined with the function keyword.
+
+function Name(parameters) {
+  // code to be executed
+}
+
+//example:
+function family(){
+  const familyMembers = ["mother" , "father" , "brother" , "sister"]
+}
+
+//And this is how you call a javascript function:
+
+functionName():
+
+//example:
+
+family():
+//This will return mother,father,brother,sister in the console
