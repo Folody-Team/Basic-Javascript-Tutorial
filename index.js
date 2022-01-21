50c512e21da761aab57142da0ecb8432040faf37
MorgTheC <91877913+MorgTheC@users.noreply.github.com>
2022-01-21 20:42:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,32 @@
 */
  
 //This is how you declare a variable for your project:
+
  var variables = "this is a variable"; //Put the semicolon at the end because is necessary
+
 /*Code breakdown
 *The variable "variables" will have the value of "this is a variable"
 */
 
 //This is a way to print the value of the 'variables' variable:
+
 console.log(variables);
-//The output will be : this is a variable
+//The output will be : this is a variable 
+
+
+
+//you can use let and const for declaring variable too
+//example:
+
+const a = 1; 
+//or :
+
+let b = 5;
+
+//danger note: let and const cannot be redeclare 
+//error example:
+let x = "x1";
+
+let x = 0;
+
+// Error message : SyntaxError: 'x' has already been declared
