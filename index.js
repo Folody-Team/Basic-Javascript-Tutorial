b9d11c645230e592e1338c0802b136bc1af63c8d
MorgTheC <91877913+MorgTheC@users.noreply.github.com>
2022-01-21 20:37:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,13 @@
 /*This is a multi line comment
  Multi line comment 2
 */
+ 
+//This is how you declare a variable for your project:
+ var variables = "this is a variable"; //Put the semicolon at the end because is necessary
+/*Code breakdown
+*The variable "variables" will have the value of "this is a variable"
+*/
 
+//This is a way to print the value of the 'variables' variable:
+console.log(variables);
+//The output will be : this is a variable
