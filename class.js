b86d7ccd31db5be5795dcb4ad51b8d83c0e0782c
Từ Thắng Phát <81029660+phatdev12@users.noreply.github.com>
2022-01-21 21:49:11 +0700

    Update and rename Class.js to class.js

---
+++ class.js
@@ -0,0 +1,16 @@
+// for example in class
+class LogName {
+
+  constructor(name) {
+    this.name = name; // name value from name
+  }
+
+  log() {
+    console.log(this.name); // log name on console
+  }
+
+}
+
+
+let user = new LogName("Folody"); // enter the name "Folody"
+user.log(); // log name
