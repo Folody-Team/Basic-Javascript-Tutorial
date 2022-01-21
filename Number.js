c3d4027259f5d4107e2a01fc3c15aee3c505bec3
MorgTheC <91877913+MorgTheC@users.noreply.github.com>
2022-01-21 21:21:43 +0700

    Update Number.js

--- Number.js
+++ Number.js
@@ -1,3 +1,5 @@
-const num = 1
-const num2 = 2
-const num3 = 3
+//In this file we will learn about some of the basic type of javascript number variable type:
+
+const num = 1 // Number without decimals
+const num2 = 2.2 //Number with decimals
+const num3 = 3e5 //Large number
