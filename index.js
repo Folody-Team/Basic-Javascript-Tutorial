c166eda2f56312ec9f58d085caa022efced1d46f
MorgTheC <91877913+MorgTheC@users.noreply.github.com>
2022-01-21 20:33:24 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+//This is a tutorial for beginner learning javascript with sample code 
+
+//This is a javascript comment , comment are the way you write instruction to your code without affect the code 
+
+/*This is a multi line comment
+ Multi line comment 2
+*/
+
